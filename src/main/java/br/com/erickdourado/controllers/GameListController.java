963407383f7870ba45d5ad97a85848392c08642a
Erick Dourado <erickdourado_@hotmail.com>
2023-05-16 10:51:05 -0300

    Move games on list and docker compose

--- src/main/java/br/com/erickdourado/controllers/GameListController.java
+++ src/main/java/br/com/erickdourado/controllers/GameListController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erickdourado.dto.GameListDTO;
 import br.com.erickdourado.dto.GameMinDTO;
+import br.com.erickdourado.dto.ReplacementDTO;
 import br.com.erickdourado.services.GameListService;
 import br.com.erickdourado.services.GameService;
 
@@ -34,4 +37,9 @@ public class GameListController {
 		return ResponseEntity.ok(gameService.findByList(listId));
 	}
 
+	@PostMapping("/{listId}/replacement")
+	public ResponseEntity<Void> move(@PathVariable Long listId, @RequestBody ReplacementDTO replacementDTO) {
+		gameListService.move(listId, replacementDTO.getSourceIndex(), replacementDTO.getDestinationIndex());
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/br/com/erickdourado/services/GameListService.java
+++ src/main/java/br/com/erickdourado/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.com.erickdourado.dto.GameListDTO;
 import br.com.erickdourado.entities.GameList;
+import br.com.erickdourado.projections.GameMinProjection;
 import br.com.erickdourado.repositories.GameListRepository;
+import br.com.erickdourado.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,10 +18,29 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameList> gameList = gameListRepository.findAll();
 		return gameList.stream().map(gl -> new GameListDTO(gl)).toList();
 	}
-
+	
+	@Transactional //Dessa vez não colocamos o "readOnly", pois não estamos buscando dados.
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> gameList = gameRepository.searchByList(listId);
+		
+		GameMinProjection game = gameList.remove(sourceIndex);//Retirei o jogo da posição padrão
+		
+		gameList.add(destinationIndex, game);//Inseri o jogo na posição de destino
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, gameList.get(i).getId(), i);
+		}
+	}
+	
 }
